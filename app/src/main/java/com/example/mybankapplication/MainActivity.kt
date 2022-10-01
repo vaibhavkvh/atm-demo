b1193f3e488b59dfe13d6387f821b2bcb8236d9a
Admin <vbv1193@gmail.com>
2022-10-01 11:29:25 +0530

    working on without database functionality

--- app/src/main/java/com/example/mybankapplication/MainActivity.kt
+++ app/src/main/java/com/example/mybankapplication/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.mybankapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import com.example.mybankapplication.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private var totalAvailableBalance = 0
     private var total2kNoteAvailable = 0
@@ -23,7 +26,100 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(activityMainBinding.root)
-      //  mActivityViewModel = ViewModelProvider(this).get(ActivityViewModel::class.java)
+        //  mActivityViewModel = ViewModelProvider(this).get(ActivityViewModel::class.java)
+
+        initViewObject()
 
     }
+
+    private fun initViewObject() {
+        activityMainBinding.apply {
+            btnSave.setOnClickListener(this@MainActivity)
+        }
+    }
+
+
+    private fun saveInitialBalance() {
+
+        if (activityMainBinding.editText.readText().isEmpty()) {
+            showMessage("Please enter balance")
+        } else if (!activityMainBinding.editText.readText().isDigitsOnly()) {
+            showMessage("Please enter valid balance")
+        } else {
+            totalAvailableBalance = activityMainBinding.editText.readText().toInt()
+            activityMainBinding.editText.viewGone()
+            activityMainBinding.btnSave.viewGone()
+            activityMainBinding.textViewAddBalance.text =
+                getString(R.string.total_balance_is,totalAvailableBalance)
+            createAvailableNotes()
+        }
+    }
+
+    private fun createAvailableNotes() {
+
+        when (totalAvailableBalance) {
+            in 0..10 -> {
+                total10NoteAvailable = totalAvailableBalance / 10
+            }
+            in 11..20 -> {
+                total10NoteAvailable = totalAvailableBalance / 10
+                total20NoteAvailable = totalAvailableBalance / 20
+            }
+            in 21..50 -> {
+                total10NoteAvailable = totalAvailableBalance / 10
+                total20NoteAvailable = totalAvailableBalance / 20
+                total50NoteAvailable = totalAvailableBalance / 50
+
+            }
+            in 51..100 -> {
+                total10NoteAvailable = totalAvailableBalance / 10
+                total20NoteAvailable = totalAvailableBalance / 20
+                total50NoteAvailable = totalAvailableBalance / 50
+                total100NoteAvailable = totalAvailableBalance / 100
+            }
+            in 101..500 -> {
+                total10NoteAvailable = totalAvailableBalance / 10
+                total20NoteAvailable = totalAvailableBalance / 20
+                total50NoteAvailable = totalAvailableBalance / 50
+                total100NoteAvailable = totalAvailableBalance / 100
+                total500NoteAvailable = totalAvailableBalance / 500
+
+            }
+            else -> {
+                total10NoteAvailable = totalAvailableBalance / 10
+                total20NoteAvailable = totalAvailableBalance / 20
+                total50NoteAvailable = totalAvailableBalance / 50
+                total100NoteAvailable = totalAvailableBalance / 100
+                total500NoteAvailable = totalAvailableBalance / 500
+                total2kNoteAvailable = totalAvailableBalance / 2000
+            }
+        }
+    }
+
+    private fun showMessage(msg: String) {
+        Snackbar.make(
+            activityMainBinding.editText,
+            msg,
+            Snackbar.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onClick(view: View?) {
+        activityMainBinding.apply {
+            when (view) {
+                btnSave -> {
+                    saveInitialBalance()
+                }
+            }
+        }
+
+    }
+}
+
+private fun View.viewGone() {
+    visibility = View.GONE
+}
+
+private fun TextView.readText(): String {
+    return this.text.toString()
 }
