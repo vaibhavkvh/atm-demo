77f8379467fd1cd88a55b0f9feb508bb408fcbb9
Admin <vbv1193@gmail.com>
2022-10-01 10:59:08 +0530

    working on without database functionality

---
+++ app/src/main/java/com/example/mybankapplication/ActivityViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.mybankapplication
+
+import androidx.lifecycle.ViewModel
+
+class ActivityViewModel : ViewModel() {
+}
--- app/src/main/java/com/example/mybankapplication/MainActivity.kt
+++ app/src/main/java/com/example/mybankapplication/MainActivity.kt
@@ -3,14 +3,27 @@ package com.example.mybankapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.mybankapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var totalAvailableBalance = 0
+    private var total2kNoteAvailable = 0
+    private var total500NoteAvailable = 0
+    private var total100NoteAvailable = 0
+    private var total50NoteAvailable = 0
+    private var total20NoteAvailable = 0
+    private var total10NoteAvailable = 0
     private lateinit var activityMainBinding: ActivityMainBinding
+
+    //private lateinit var mActivityViewModel: ActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(activityMainBinding.root)
+      //  mActivityViewModel = ViewModelProvider(this).get(ActivityViewModel::class.java)
+
     }
 }
